366f63dd02ea403a1fb015ff5b3038b20f23b853
Inemesit <inemesit.e.matthew@gmail.com>
2023-11-02 00:24:36 +0000

    Task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# Extract and print the desired sentence
+print(str[39:66])
