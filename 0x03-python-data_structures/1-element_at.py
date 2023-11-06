67fd9e7a8e54259d082f345f704eb3d25153310c
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-06 20:59:08 +0100

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,10 +4,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx >= len(my_list):
-        return None
-    element = my_list[idx]
-    return element
+    return None if idx >= len(my_list) else my_list[idx]
 
 
 # Example usage
