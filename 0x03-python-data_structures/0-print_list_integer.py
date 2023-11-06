3df16e2341ed60a747470188b93b16a5f095406d
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-06 20:40:02 +0100

    Task 0 error fixed

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for item in my_list:
         print("{:d}".format(item))
-# Example usage
+
+
+# Two blank lines after the function definition
+
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
+
+# Add a newline at the end of the file
