3f8ed43878d81f7a6aa7bdab5547fedc9fe568c8
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-07 19:16:04 +0100

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+
+def divisible_by_2(my_list=None):
+    if my_list is None:
+        my_list = []
+    result = []
+
+    for number in my_list:
+        if number % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return result
+
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_result = divisible_by_2(my_list)
+
+    i = 0
+    while i < len(list_result):
+        print(
+            "{:d} {:s} divisible by 2".format(
+                my_list[i], "is" if list_result[i] else "is not"
+            )
+        )
+        i += 1
