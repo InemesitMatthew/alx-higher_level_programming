47804cc261e4b17c89a52fca76ae9d73a52ffe2a
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-09 19:36:27 +0100

    nth

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-import hidden_4
 
-# Get all the names defined in hidden_4 module
-module_names = dir(hidden_4)
 
-# Filter and print the names that do not start with double underscores
-for name in sorted(module_names):
-    if not name.startswith("__"):
-        print(name)
+# # Get all the names defined in hidden_4 module
+# module_names = dir(hidden_4)
+
+# # Filter and print the names that do not start with double underscores
+# for name in sorted(module_names):
+#     if not name.startswith("__"):
+#         print(name)
+
+if __name__ == '__main__':
+    import hidden_4
+    words = dir(hidden_4)
+
+
+    for word in words:
+        if word[:2] != '__':
+            print(word)
