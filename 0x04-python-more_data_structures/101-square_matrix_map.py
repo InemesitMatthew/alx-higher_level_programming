aef9b3b0740c6db4a9a4e3882eca26b1afb0f98e
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-09 19:20:08 +0100

    Task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=None):
-    if matrix is None:
-        matrix = []
+def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-
-
-# Example usage
-if __name__ == "__main__":
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    new_matrix = square_matrix_map(matrix)
-    print(new_matrix)
-    print(matrix)
