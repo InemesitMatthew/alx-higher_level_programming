390e3b7b18470f71ecceb697ec7d3193298c7832
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-20 20:03:50 +0100

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        if result is not None:
+            print("Inside result: {}".format("{:.1f}".format(result)))
+        else:
+            print("Inside result: {}".format(result))
+
+    return result
+
+# Test the function with the provided examples
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
