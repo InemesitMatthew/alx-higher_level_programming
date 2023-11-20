002da1a879e47f23a064e6fd3e5ef75eb3f4c9e2
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-20 23:04:27 +0100

    Task 5

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    raise TypeError("Exception raised")
+
+
+# Test the function with the provided example
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
