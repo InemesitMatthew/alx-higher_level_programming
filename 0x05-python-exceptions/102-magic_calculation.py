8615ef6c9525146678a53fc56fd338d2a1aba33d
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-11-21 01:16:57 +0100

    Task 9 fix

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,15 +1,23 @@
+#!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
-    
+
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise Exception("Too far")
         except Exception as e:
             print(e)
             raise
-        
-        result = result + (a ** b) / i
-    
+
+        result = result + (a**b) / i
+
     result = result + a + b
     return result
+
+
+# Disassembling the bytecode
+import dis
+
+dis.dis(magic_calculation)
