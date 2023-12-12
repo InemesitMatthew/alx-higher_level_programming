d77d93f177c1741e88b660a38e0c1364f3f541a6
InemesitMatthew <inemesit.e.matthew@gmail.com>
2023-12-12 05:05:52 +0100

    Dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize the Base class."""
+        """Initialize Base instance."""
         if id is not None:
             self.id = id
         else:
@@ -24,7 +24,6 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-
     @classmethod
     def save_to_file(cls, list_objs):
         """Save a list of instances to a file in JSON format."""
@@ -32,7 +31,7 @@ class Base:
         json_list = []
         if list_objs is not None:
             json_list = [obj.to_dictionary() for obj in list_objs]
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             file.write(cls.to_json_string(json_list))
 
     @staticmethod
@@ -41,3 +40,16 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance with attributes set from a dictionary."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+
+        dummy.update(**dictionary)
+        return dummy
--- 0x0C-python-almost_a_circle/test.py
+++ 0x0C-python-almost_a_circle/test.py
@@ -361,3 +361,19 @@
 #     print("[{}] {}".format(type(list_input), list_input))
 #     print("[{}] {}".format(type(json_list_input), json_list_input))
 #     print("[{}] {}".format(type(list_output), list_output))
+
+
+
+#!/usr/bin/python3
+""" 17-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(3, 5, 1)
+    r1_dictionary = r1.to_dictionary()
+    r2 = Rectangle.create(**r1_dictionary)
+    print(r1)
+    print(r2)
+    print(r1 is r2)
+    print(r1 == r2)
