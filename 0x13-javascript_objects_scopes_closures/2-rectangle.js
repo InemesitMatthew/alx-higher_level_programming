1ea1551192d99a9b38a1a2aece505f6a5b3a870e
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-02-27 02:48:03 +0100

    lets hope

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
-    constructor(w, h) {
+module.exports = class Rectangle {
+    constructor (w, h) {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
-      } else {
-        // If w or h is equal to 0 or not a positive integer, create an empty object
-        this.width = undefined;
-        this.height = undefined;
       }
     }
-  }
-  
-  module.exports = Rectangle;  
+  };
