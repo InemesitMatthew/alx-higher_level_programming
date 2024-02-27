ade85d8cd4e060c5b0987a65fc93bd405d7f047d
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-02-27 03:35:50 +0100

    factor

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const { list } = require('./100-data');
-
-const newList = list.map((value, index) => value * index);
+const list = require('./100-data').list;
 
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
